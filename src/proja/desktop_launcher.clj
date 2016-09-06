eef0942b113cd32ff8b95aa9b0271de422d71440
chris <no@ok.com>
2016-09-06 18:36:18 -0400

    Start belt-grouping logic.

--- src/proja/desktop_launcher.clj
+++ src/proja/desktop_launcher.clj
@@ -90,32 +90,9 @@
 ;  if group has an end, if end has something to move, stall. don't move, don't run the group.
 ;  else call move for each belt in the group.
 ;
-;every time a belt is added, re-calc the groups and ends
-;if have current group
-; get last belt from group list
-; look at n/s/e/w,
-; if belt is facing this or same direction as this or this is facing it
-;    and belt is not in group and belt is not facing this
-; then valid-belt-neighbor-count++ (for this)
-;
-; if don't have current group
-;  then create group, add belt to group, and use it for the logic
-;
-; if count == 1
-;  then add that belt to group
-; else if count > 1
-;  then search for first neighbor, clockwise, starting from 0 degrees / north
-;  add it to group
-;  put rest on intersection list for group
-; else if count == 0
-;  then if intersection count > 0 take first, add to group. remove from intersection
-;  if intersection count == 0, then we're done.
-;
-;find ends
-; run this right after finding a group
-; for each belt in group
-; if belt has no neighbor that is facing away, then it's an end, done
-; else next belt
+
+
+
 ;
 ;change to ecs to allow systems that are given all qualified entities, instead of one at a time
 ;set a flag or something to dictate which one the run function is expecting.
